579cf54ee53bda3c7f08ccaa02515bd7ff8d5dad
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-01-08 21:44:34 -0300

    feat: Styles status bar

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import {
   useFonts,
   Roboto_400Regular,
@@ -14,6 +14,11 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={Theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
