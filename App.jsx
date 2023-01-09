9fd9e7b0a8bc0dd54302ad45cefc5bf7f7744c56
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-01-09 14:56:56 -0300

    chore: create screen details

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
-import Register from './src/Screens/Register';
+import Details from './src/Screens/Details';
 import Loading from './src/components/Loading';
 import { Theme } from './src/styles/theme';
 
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Register /> : <Loading />}
+      {fontsLoaded ? <Details /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 <img src="tela-sign-in.png" width="220px"/>
 <img src="tela-home.png" width="220px"/>
+<img src="tela-register.png" width="220px"/>
